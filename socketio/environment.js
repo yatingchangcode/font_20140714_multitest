ece9df76e7c2ebe477909e5d448b87b931c774bf
motephyr <motephyr@gmail.com>
2015-06-05 14:48:19 +0800

    split the controller code.
    Former-commit-id: 3dedbdcf34958732b2e8c05e013cee1d51136727

--- socketio/environment.js
+++ socketio/environment.js
@@ -5,11 +5,8 @@ module.exports = {
     loadSocketIo: function loadSocketIo() {
 
         var client_id = {};
-        var emitUserId = function(user_id,message_name,message){
-            for (var x in client_id[user_id]){
-                io.to(client_id[user_id][x]).emit(message_name,message)
-            }
-        }
+
+
 
         var port = process.env.PORT || 5001;
         if (process.env.NODE_ENV != 'production') {
@@ -21,6 +18,11 @@ module.exports = {
         var io = require('socket.io').listen(Number(port));
 
         io.on('connection', function(socket) {
+            var emitUserId = function(user_id,message_name,message){
+                for (var x in client_id[user_id]){
+                    io.to(client_id[user_id][x]).emit(message_name,message)
+                }
+            }
             console.log(socket.request.session);
             var currentSocketIoUserId = socket.request.session['user_id'];
 
@@ -30,87 +32,7 @@ module.exports = {
             client_id[currentSocketIoUserId].push(socket.id);
 
 
-            (function loadFont(){
-                var game_id,stage_name,visitors;
-
-                socket.on('down_location',function(msg){
-                    emitUserId('0','down_location',msg);
-                });
-
-                socket.on('move_location',function(msg){
-                    emitUserId('0','move_location',msg);
-                });
-
-                socket.on('up_location',function(msg){
-                    emitUserId('0','up_location',msg);
-                });
-
-                //
-                socket.on('clear', function(msg){
-                    socket.broadcast.emit('clear',msg);
-                })
-
-                //
-                socket.on('clearAll',function(){
-                    socket.broadcast.emit('clear');
-                })
-
-                socket.on('submit',function(msg){
-                    emitUserId('0','submit',msg);
-                });
-
-                socket.on('cancelSubmit',function(msg){
-                    emitUserId('0','cancelSubmit',msg);
-                });
-
-                //
-                socket.on('set_gameinfo_to_socket',function(msg){
-                    game_id = msg.game;
-                    stage_name = msg.stage;
-                    visitors = msg.visitors.split(",");
-                });
-
-                //
-                socket.on('action',function(msg){
-                    emitUserId(msg.user_id,'action', msg);
-                    emitUserId('0','action',msg);
-                });
-
-                socket.on('right',function(msg){
-                    emitUserId('0','right',msg.user_id);
-                });
-
-                socket.on('removeO',function(msg){
-                    emitUserId('0','removeO',msg.user_id);
-                });
-
-                socket.on('reset',function(msg){
-                    socket.broadcast.emit('reset',msg);
-                });
-
-                //
-                socket.on('continue_write',function(msg){
-                    emitUserId(msg.user_id,'continue_write', msg);
-                    emitUserId('0','continue_write',msg);
-                });
-
-                socket.on('setCorrectCount',function(msg){
-                    emitUserId('0','setCorrectCount',msg);
-                });
-
-                socket.on('showCorrectUsers',function(msg){
-                    emitUserId('0','showCorrectUsers',msg);
-                });
-
-                socket.on('userOut',function(msg){
-                    emitUserId('0','userOut',msg);
-                });
-
-
-
-
-
-            })();
+            socket = require('./controller/write_controller')(socket,emitUserId);
 
             socket.on('disconnect', function(message) {
                 console.log("which user id "+currentSocketIoUserId);
