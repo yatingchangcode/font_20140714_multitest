4017c9cb1ec91614d17efbd928a8804d308e22fd
KinoLien <miecowbai@gmail.com>
2016-12-18 13:50:57 +0800

    Add set colors to timebar.
    Former-commit-id: d31adfbbc25099082fa915a2bcc2288697df3eb1

--- app/assets/javascripts/timebar.js
+++ app/assets/javascripts/timebar.js
@@ -2,6 +2,44 @@
 window.TimeBar = (function(){
   var _instances = {};
 
+  var _converToSix = function(color){
+    if(color.length == 3){
+      color = color[0] + color[0] + color[1] + color[1] + color[2] + color[2];
+    }
+    return color;
+  };
+
+  function colorMgr(from, to){
+    var strFromColor = _converToSix(from.substr(1));
+    var strToColor = _converToSix(to.substr(1));
+    
+    var redFrom = parseInt(strFromColor.substring(0, 2), 16);
+    var greenFrom = parseInt(strFromColor.substring(2, 4), 16);
+    var blueFrom = parseInt(strFromColor.substring(4), 16);
+
+    var redTo = parseInt(strToColor.substring(0, 2), 16);
+    var greenTo = parseInt(strToColor.substring(2, 4), 16);
+    var blueTo = parseInt(strToColor.substring(4), 16);
+
+    this.red = { origin: redFrom, diff: redFrom - redTo };
+    this.green = { origin: greenFrom, diff: greenFrom - greenTo };
+    this.blue = { origin: blueFrom, diff: blueFrom - blueTo };
+
+  };
+
+  colorMgr.prototype.getColor = function(percent){
+    var self = this;
+    var keys = ['red', 'green', 'blue'];
+    var res = keys.map(function(item){
+      var o = self[item], str;
+      if(o.diff == 0) str = o.origin.toString(16);
+      else str = parseInt(o.origin - o.diff * (1 - percent)).toString(16);
+      return (str.length < 2)? ("0" + str) : str;
+    });
+    return '#' + res.join('');
+  };
+
+  
   var timebarInstance = function(id){
     this._jqel = $('#'+id);
     this._int = null;
@@ -10,6 +48,9 @@ window.TimeBar = (function(){
     this._changeBegin = (1 / 3) * 100;
     this._changeEnd = (1 / 15) * 100;
     this._changeInt = this._changeBegin - this._changeEnd;
+    this._startColor = "#ffff00";
+    this._endColor = "#ff0000";
+    this._colorMgr = new colorMgr(this._startColor, this._endColor);
   };
 
   timebarInstance.prototype._stopInterval = function(){
@@ -28,7 +69,7 @@ window.TimeBar = (function(){
   timebarInstance.prototype.resetBar = function(){
     this._stopInterval();
     this._curs = this._sec;
-    this._jqel.css("width", "100%").css("background-color", '#ffff00').attr('aria-valuenow', 100);
+    this._jqel.css("width", "100%").css("background-color", this._startColor).attr('aria-valuenow', 100);
   };
 
   timebarInstance.prototype.doStop = function(toClear){
@@ -48,12 +89,14 @@ window.TimeBar = (function(){
             var percent = 100 * scope._curs / scope._sec;
             scope._jqel.css("width", percent+"%").attr('aria-valuenow', percent);
             if(percent >= scope._changeBegin){
-              scope._jqel.css("background-color", '#ffff00');
+              scope._jqel.css("background-color", this._startColor);
             }else if(percent > scope._changeEnd && percent < scope._changeBegin){
-              var percentInGreen = parseInt((percent - scope._changeEnd) / scope._changeInt * 255);
-              scope._jqel.css("background-color", '#ff'+ percentInGreen.toString(16) +'00');
+              var currentPercent = (percent - scope._changeEnd) / scope._changeInt; // from 0.9x -> 0.01x
+              // var percentInGreen = parseInt(currentPercent * 255);
+              // scope._jqel.css("background-color", '#ff'+ percentInGreen.toString(16) +'00');
+              scope._jqel.css("background-color", scope._colorMgr.getColor(currentPercent));
             }else{
-              scope._jqel.css("background-color", '#ff0000');
+              scope._jqel.css("background-color", this._endColor);
             }
           } else {
             scope.doStop();
@@ -72,6 +115,12 @@ window.TimeBar = (function(){
 
   timebarInstance.prototype.setSize = function(width, height){
 
+  };
+
+  timebarInstance.prototype.setColors = function(from, to){
+    this._startColor = _converToSix(from);
+    this._endColor = _converToSix(to);
+    this._colorMgr = new colorMgr(this._startColor, this._endColor);
   };
 
   var _getIns = function(id){
