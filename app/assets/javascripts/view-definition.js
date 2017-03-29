23b84d670d6a5aaab1522b5b8190ac49194afe6a
KinoLien <miecowbai@gmail.com>
2017-03-29 21:04:58 +0800

    Fix C2 out style.
    Former-commit-id: 96607d629ae01b6897079b0ed02aa09c8a3026b4

--- app/assets/javascripts/view-definition.js
+++ app/assets/javascripts/view-definition.js
@@ -1089,7 +1089,6 @@ View.setUserOutStyle = (function(key){
   key = Commons.getCommonGenKey(key, ["A1+console","A3+console","B1+console","B2_v1+console"]);
   key = Commons.getCommonGenKey(key, ["A1+tv","A3+tv","B1+tv","B2_v1+tv"]);
   key = Commons.getCommonGenKey(key, ["C1+tv","C3+tv"]);
-  key = Commons.getCommonGenKey(key, ["A2+tv","C2+tv"]);
   key = Commons.getCommonGenKey(key, ["mix+tv","C5+tv"]);
   return {
     // server: A1 A3 B1 B2_v1
@@ -1158,6 +1157,13 @@ View.setUserOutStyle = (function(key){
       $('#out_' + o.user_id).show();
       $('#user_photo_' + o.user_id).parent().css("opacity", 0.3);
       $('#grid_' + o.user_id).css("opacity", 0.3);
+    },
+    "C2+tv": function(o){
+      $('#out_' + o.user_id).show();
+      $('#user_photo_' + o.user_id).parent().css("opacity", 0.3);
+      $('#grid_' + o.user_id).css("opacity", 0.3);
+      $('#sketchSecond_' + o.user_id).parent().css("opacity", 0.3);
+      Commons.sketchSecondIns[o.user_id].clearBar();
     }
   }[key] || Commons.emptyFn;
 })(Settings.genKey);
